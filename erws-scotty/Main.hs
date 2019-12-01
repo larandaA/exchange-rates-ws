48652d451476d32547faf68599a1054460f9a3d9
Alexandra Ryzhevich <larandaansil@gmail.com>
2019-12-01 23:30:10 +0300

    Add json http hello world with scotty

--- erws-scotty/Main.hs
+++ erws-scotty/Main.hs
@@ -2,10 +2,20 @@
 
 import qualified Data.Aeson as Aeson
 import Data.Aeson ((.=))
+import Network.Wai
+import Network.Wai.Cli
 import Web.Scotty
 
+
+app :: IO Application
+app = scottyApp $ do
+    get "/hello/:word" $ do
+        word <- param "word"
+        json $ Aeson.object ["hello" .= (word :: String)]
+    notFound $ do
+        path <- rawPathInfo <$> request
+        json $ Aeson.object ["error" .= ("The path does not exist: " ++ show path)]
+
+
 main :: IO ()
-main = scotty 3000 $ do
-  get "/hello/:word" $ do
-    word <- param "word"
-    json $ Aeson.object ["hello" .= (word :: String)]
+main = app >>= defWaiMain
